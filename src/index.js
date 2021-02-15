151db43d876e4f87da0bb98601dc3a8933448301
eternal <73202350+slow@users.noreply.github.com>
2021-02-15 21:28:07 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const { Util: djsUtil } = require('discord.js');
 
 async function init() {
-   // Requires
+    // Requires
+   const Constants = require('./lib/Constants');
+   const Webhook = require('./lib/Webhook');
+   const Logger = require('./lib/Logger');
+   const modes = require('./modes/index');
+   const Util = require('./lib/Util');
+   const chalk = require('chalk');
+   const phin = require('phin');
+
    // Call dotenv to recognize env vars
+   require('dotenv').config();
    // Define globals
    // Try to parse settings
    // Define settings with defaults
    // Init selected mode
    // Counters
    // Get payment method
-   //  Init webhook
+   // Init webhook
 }
 
 init();
