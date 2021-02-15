bbf5bcd70a8e34181a342ff355b737d09b0347a5
eternal <73202350+slow@users.noreply.github.com>
2021-02-15 21:52:49 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,10 +43,32 @@ async function init() {
    global.util = Util;
    global.logger = new Logger({ debug: false });
    global.paymentId = null;
+
    // Try to parse settings
+   try {
+      global.settings = JSON.parse(process.env.settings);
+   } catch {
+      return logger.critical(constants.invalidConfig);
+   }
+
    // Define settings with defaults
+   global.settings = djsUtil.mergeDefault(constants.defaultSettings, settings);
+
+   if (!settings.mode) return logger.critical(constants.noMode);
+   if (!Object.keys(modes).includes(settings.mode)) return logger.critical(constants.invalidMode);
+
    // Init selected mode
+   logger.debug(constants.initSniper);
+   await modes[settings.mode]();
+
+   if (!snipers.length) return logger.critical(constants.invalidTokens);
+
    // Counters
+   let guildCount = snipers
+      .map((s) => s.guilds.cache.size)
+      .reduce((a, b) => a + b, 0);
+
+   let sniperCount = snipers.length;
    // Get payment method
    // Init webhook
 }
