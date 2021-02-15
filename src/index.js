12cbf3c04b54713a506f240bf95e5841e41054ea
eternal <73202350+slow@users.noreply.github.com>
2021-02-15 20:51:02 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const { Util: djsUtil } = require('discord.js');
+
+async function init() {
+   // Requires
+   const Constants = require('./lib/Constants');
+   const Webhook = require('./lib/Webhook');
+   const Logger = require('./lib/Logger');
+   const modes = require('./modes/index');
+   const Util = require('./lib/Util');
+   const chalk = require('chalk');
+   const phin = require('phin');
+
+   // Call dotenv to recognize env vars
+   require('dotenv').config();
+
+   console.log(chalk.green(`
+                                           ╓╖,
+               g▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄╬╣╣╣╢╬N╖
+              ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╣╣╣╣╣╣╣╣╣╢╣╣@,
+               ▀▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╣╣╣╣╣╣╣╢╢╣╣╣╣╣@,
+                        ╙▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╣╣╣╣╣╣╢╣╣╣╣╣╣╣╣╣╣W
+                        g▓▓▓▓▓▓╢▓▓▓▓╩╨╨╩╬╣╢╣╢╣▒╣╣╣╣╣╣╣╣╣╣╣╣
+ Æ▓▓▓▓⌐   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╢▓▓╝⌠░░░░░░░░░░╙╨╣╣╣╣╣╣╣╣╣╣╣╣╣╣╣
+ ╙▀▓▓▀    ╙▀▓▓▓▓▓▓▓▓▓▓▓▓▓▓╢▓▓╩░░░░░░░░░░░░░░░░╙╣╣╣╣╣╣╣╣╣╣╣╣╣@
+                     ▓▓▓▓╢▓▓░░░░░╓╢▒▒▒▒▒▒╢░░░░░╙╣╣╣╣╣╣╣╣╣╣╣╣╢
+               g▄▄▄▄▄▓▓▓▓▓▓Ñ░░░░║▒▒▒▒▒▒▒▒▒▒╖░░░░║╣╣╣╣╣╣╣╣╣╣╣╣⌐
+              ▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░║▒▒▒▒▒▒▒▒▒▒▒▒▒░░░]╣╣╣╣╣╣╣╣╣╣╣╣⌐
+               ╙╙▐▓▓▓▓▓▓▓▓▓@░░░░╙▒▒▒▒▒▒▒▒▒▒╜░░░░║╣╣╣╣╣╣╣╣╣╣╣╣
+                  ▓▓▓▓▓▓▓╣▓▓░░░░░╙▒▒▒▒▒▒▒▒░░░░░╓╣╣╣╣╣╣╣╣╣╣╣╣Ñ
+                  ╚▓▓▓▓▓▓▓▓▓▓@░░░░░░░░░░░░░░░░░░░╢╣╣╣╣╣╣╣╣╣╣
+                   ▐▓▓▓▓▓▓▓▓╢▓▓@µ░░░░░░░░░░╓@░░░░░░╙╣╣╣╣╣╣╢\`
+                    ╙▓▓▓▓▓▓▓▓▓▓╢▓▓▓▓@@@@▓▓▓▓╢▓▄░░░░░░░╜╨╣╢░
+                      ▀▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄░░░░░░░▒
+                         ▀▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╜\"░▒░\"
+                           ▀▀▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀╩
+                               ╙╙▀▀▀▓▓▓▓▀▀▀╙╙
+   `));
+
+   // Define globals
+   global.snipers = [];
+   global.webhook = null;
+   global.constants = Constants;
+   global.util = Util;
+   global.logger = new Logger({ debug: false });
+   global.paymentId = null;
+
+   // Try to parse settings
+   try {
+      global.settings = JSON.parse(process.env.settings);
+   } catch {
+      return logger.critical(constants.invalidConfig);
+   }
+
+   // Define settings with defaults
+   global.settings = djsUtil.mergeDefault(constants.defaultSettings, settings);
+
+   if (!settings.mode) return logger.critical(constants.noMode);
+   if (!Object.keys(modes).includes(settings.mode)) return logger.critical(constants.invalidMode);
+
+   // Init selected mode
+   logger.debug(constants.initSniper);
+   await modes[settings.mode]();
+
+   if (!snipers.length) return logger.critical(constants.invalidTokens);
+
+   // Counters
+   let guildCount = snipers
+      .map((s) => s.guilds.cache.size)
+      .reduce((a, b) => a + b, 0);
+
+   let sniperCount = snipers.length;
+}
+
+init();
